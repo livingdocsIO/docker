cbfb92425ca5f241c8b60e5c94138aa485d5e9b5
Marc Bachmann <marc.brookman@gmail.com>
2020-04-23 18:10:08 +0200

    feat: Update pgbouncer to properly support an auth query with minimal configurations

--- pgbouncer/setup_password_lookup_function.sql
+++ pgbouncer/setup_password_lookup_function.sql
@@ -1,8 +1,15 @@
+# docker run --name=pgbouncer -d -e PGBOUNCER_UPSTREAM_USER=user_pgbouncer -e PGBOUNCER_UPSTREAM_PASSWORD=md58b9ad77b2409cd985b672a95d652b369 -e PGBOUNCER_UPSTREAM='host=172.17.0.1 port=5432' -p 5433:5432 livingdocs/pgbouncer
+
+# Execute this in the template1 database
+CREATE SCHEMA pooler;
+REVOKE ALL PRIVILEGES ON SCHEMA pooler FROM PUBLIC;
+
 CREATE ROLE role_pgbouncer NOLOGIN NOINHERIT;
 CREATE ROLE user_pgbouncer WITH LOGIN ENCRYPTED PASSWORD 'some-password' INHERIT;
 GRANT role_pgbouncer to user_pgbouncer;
 
-CREATE OR REPLACE FUNCTION public.pgbouncer_lookup_user(
+# And this for every existing database and in the template1 database
+CREATE OR REPLACE FUNCTION pooler.pooler_lookup_user(
   in i_username text,
   in i_excludename text,
   out uname text,
@@ -18,5 +25,5 @@ CREATE OR REPLACE FUNCTION public.pgbouncer_lookup_user(
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
 -- make sure only "role_pgbouncer" can use the function
-REVOKE EXECUTE ON FUNCTION public.pgbouncer_lookup_user(name) FROM PUBLIC;
-GRANT EXECUTE ON FUNCTION public.pgbouncer_lookup_user(name) TO role_pgbouncer;
+REVOKE ALL ON FUNCTION pooler.pooler_lookup_user(text, text) FROM PUBLIC;
+GRANT EXECUTE ON FUNCTION pooler.pooler_lookup_user(text, text) TO role_pgbouncer;
